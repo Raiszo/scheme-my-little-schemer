8a9ffa94d3ca8cb0f52cdd68ae4f4ade99b97cda
peppa-peddler <peppasama@gmail.com>
2016-09-12 13:36:16 -0500

    Adding chapter 1

---
+++ 1_Toys/List_basics.clj
@@ -0,0 +1,10 @@
+; The equivalent of 'car' and 'cdr' in clojure
+; are the functions 'first' and 'rest'
+; respectively
+
+(def car first)
+
+(def cdr rest)
+
+; However we will keep the function 'first' and
+; 'rest' for convenience
---
+++ 1_Toys/cons.clj
@@ -0,0 +1,22 @@
+; There is some misconception about
+; the constructors in the data
+; collections.
+;
+; To summarize, here is a little
+; statement writen by a guy named
+; 'Michal Marczyk' in stack overflow
+; concerning the use of cons vs conj
+;
+;     "The intention behind the names is,
+;     I believe, that cons means to cons(truct aseq), whereas conj
+;     means to conj(oin an item onto a collection). The seq being
+;     constructed by cons starts with the element passed as its first
+;     argument and has as its next / rest part the thing resulting from
+;     the application of seq to the second argument; as displayed above,
+;     the whole thing is of class clojure.lang.Cons. In contrast, conj
+;     always returns a collection of roughly the same type as the
+;     collection passed to it."
+
+; So we will keep the function 'cons'.
+
+(cons 1 '(1 2 3 4))
