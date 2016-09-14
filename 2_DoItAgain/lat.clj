ce39edb41b585831882bc9a677f8979dc28feb35
peppa-peddler <peppasama@gmail.com>
2016-09-14 17:55:16 -0500

    Adding comment

--- 2_DoItAgain/lat.clj
+++ 2_DoItAgain/lat.clj
@@ -1,3 +1,12 @@
+;Here we use the first commandment
+;of the little schemer.
+;
+;Instead of using 'null?',
+;empty? is used.
+
+;Also we use 'cond' as we would in
+;Scheme.
+
 (def lat?
   (fn [x]
     (cond
