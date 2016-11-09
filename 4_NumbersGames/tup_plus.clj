e25088fc82c3cbd26c2914b54b3284805e416470
peppa-peddler <peppasama@gmail.com>
2016-11-09 16:05:42 -0500

    fixing tup_plus

--- 4_NumbersGames/tup_plus.clj
+++ 4_NumbersGames/tup_plus.clj
@@ -1,6 +1,8 @@
 (def tup+
   (fn [l1 l2]
-    (when (seq l1)
-      (cons
-        (+ (first l1) (first l2))
-        (tup+ (rest l1) (rest l2))))))
+    (cond
+      (empty? l1) l2
+      (empty? l2) l1
+      :else (cons
+              (+ (first l1) (first l2))
+              (tup+ (rest l1) (rest l2))))))
