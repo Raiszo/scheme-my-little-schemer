fc2e956813e199f1949e001bdc91dfef2f646bec
peppa-peddler <peppasama@gmail.com>
2016-11-19 19:04:32 -0500

    Adding pairs

---
+++ 7_FriendsAndRelations/pairs.clj
@@ -0,0 +1,12 @@
+(defn a-pair? [l]
+  (cond
+    (not (seq? l)) false
+    (empty? l) false
+    (empty? (rest l)) false
+    (empty? (rest (rest l))) true
+    :else false))
+
+(def fst #(first %))
+(def snd #(first (rest %)))
+(defn build [s1 s2]
+  (cons s1 (cons s2 '())))
